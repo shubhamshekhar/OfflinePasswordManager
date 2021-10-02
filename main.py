5bdf9751f5dbeb51bdc03d930eb2109f6f900b89
Shubham Shrivastava <shubhamshekharshrivastava@gmail.com>
2021-10-03 02:06:15 +0530

    Formatted code structure

--- main.py
+++ main.py
@@ -92,44 +92,43 @@ def save():
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+if __name__ == "__main__":
+    window = Tk()
+    window.title("Offline Password Manager")
+    window.config(padx=50, pady=50)
 
-window = Tk()
-window.title("Offline Password Manager")
-window.config(padx=50, pady=50)
+    canvas = Canvas(window, width=200, height=200)
+    image = PhotoImage(file="logo.png")
+    canvas.create_image(100, 100, image=image)
+    canvas.grid(row=0, column=1)
 
-canvas = Canvas(window, width=200, height=200)
-image = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=image)
-canvas.grid(row=0, column=1)
+    website_label = Label(text="Website:")
+    website_label.grid(row=1, column=0)
 
-website_label = Label(text="Website:")
-website_label.grid(row=1, column=0)
+    website_text = Entry(width=34)
+    website_text.grid(row=1, column=1, columnspan=1, pady=10)
+    website_text.focus()
 
-website_text = Entry(width=34)
-website_text.grid(row=1, column=1, columnspan=1, pady=10)
-website_text.focus()
+    search_password_button = Button(text="Search Password", command=find_password)
+    search_password_button.grid(row=1, column=2, pady=10)
 
-search_password_button = Button(text="Search Password", command=find_password)
-search_password_button.grid(row=1, column=2, pady=10)
+    email_label = Label(text="Email/Username:")
+    email_label.grid(row=2, column=0)
 
+    email_text = Entry(width=52)
+    email_text.grid(row=2, column=1, columnspan=2)
+    email_text.insert(0, "shubhamshekhar@gmail.com")
 
-email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0)
+    password_label = Label(text="Password:")
+    password_label.grid(row=3, column=0)
 
-email_text = Entry(width=52)
-email_text.grid(row=2, column=1, columnspan=2)
-email_text.insert(0, "shubhamshekhar@gmail.com")
+    password_text = Entry(width=34)
+    password_text.grid(row=3, column=1)
 
-password_label = Label(text="Password:")
-password_label.grid(row=3, column=0)
+    generate_password_button = Button(text="Generate Password", command=generate_password)
+    generate_password_button.grid(row=3, column=2, pady=10)
 
-password_text = Entry(width=34)
-password_text.grid(row=3, column=1)
+    add_button = Button(text="Add", width=42, command=save)
+    add_button.grid(row=4, column=1, columnspan=2, pady=10)
 
-generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2, pady=10)
-
-add_button = Button(text="Add", width=42, command=save)
-add_button.grid(row=4, column=1, columnspan=2, pady=10)
-
-window.mainloop()
+    window.mainloop()
